2ba52fe6bf40c6624896d50597b209a97353026b
Mugisha Joshua <mugishajoshua567@gmail.com>
2026-04-29 17:38:19 +0200

    Feat: Add IdempotencyRecord model with migrations

--- idempotency_gateway/payments/models.py
+++ idempotency_gateway/payments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class IdempotencyRecord(models.Model):
+    idempotency_key = models.CharField(max_length=255, unique=True)
+    request_body = models.JSONField()
+    response_body = models.JSONField(null=True, blank=True)
+    status_code = models.IntegerField(null=True, blank=True)
+    is_processing = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__ (self):
+        return self.idempotency_key
